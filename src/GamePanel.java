0e137393bc582d832b1dabae2abd37ea1235baf4
Nico <nico95.nm@gmail.com>
2024-07-16 23:51:28 +0200

    Start new project

---
+++ src/GamePanel.java
@@ -0,0 +1,21 @@
+
+
+import javax.swing.*;
+
+public class GamePanel extends JPanel {
+
+    // SCREEN SETTINGS
+    final int originalTileSize = 16;
+    final int scale = 3;
+
+
+    final int tileSize= originalTileSize *scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenRow;
+
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Testing game");
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+
+    }
+}

5fe962846bf6feda5cb6d436a1e6f60c56a64b09
LazyMan95 <nico95.nm@gmail.com>
2024-07-17 10:52:38 +0200

    Update player movement

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -16,11 +16,24 @@ public class GamePanel extends JPanel implements Runnable {
     final int screenWidth = tileSize * maxScreenRow; // 768px
     final int screenHeight = tileSize * maxScreenRow; // 576px
 
+    // Set players default position
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+
+
+
 public GamePanel() {
+    // SCREEN SETTINGS
     this.setPreferredSize(new Dimension(screenWidth, screenHeight));
     this.setBackground(Color.black);
     this.setDoubleBuffered(true);
+    this.addKeyListener(keyH);
+    this.setFocusable(true);
 }
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
 
 public void startGameThread(){
@@ -41,7 +54,21 @@ public void startGameThread(){
         }
 }
 
-public void update(){}
+public void update(){
+    if(keyH.upPressed == true){
+        playerY -= playerSpeed;
+        // same
+        playerY = playerY - playerSpeed;
+    } else if (keyH.downPressed == true){
+        playerY += playerSpeed;
+    } else if (keyH.leftPressed == true) {
+        playerX -= playerSpeed;
+    } else if (keyH.rightPressed == true) {
+        playerX += playerSpeed;
+    }
+
+
+}
 public void paintComponent(Graphics g){
     super.paintComponent(g);
 
@@ -49,7 +76,7 @@ public void paintComponent(Graphics g){
 
     g2.setColor(Color.white);
 
-    g2.fillRect(100, 100, tileSize, tileSize);
+    g2.fillRect(playerX, playerY, tileSize, tileSize);
 
     g2.dispose();
 
--- src/KeyHandler.java
+++ src/KeyHandler.java
@@ -31,6 +31,19 @@ public boolean upPressed, downPressed, leftPressed, rightPressed;
 
     @Override
     public void keyReleased(KeyEvent e) {
-
+int code = e.getKeyCode();
+        if(code == KeyEvent.VK_W){
+            upPressed = false;
+        }
+        if(code == KeyEvent.VK_A){
+            leftPressed = false
+            ;
+        }
+        if(code == KeyEvent.VK_S){
+            downPressed = false;
+        }
+        if(code == KeyEvent.VK_D){
+            rightPressed = false;
+        }
     }
 }

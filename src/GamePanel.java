ad2541bf9a60aeaef900e5fe2536d604910cac8c
LazyMan95 <nico95.nm@gmail.com>
2024-07-17 10:23:36 +0200

    Create player movement

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,21 +1,59 @@
 
 
 import javax.swing.*;
+import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     // SCREEN SETTINGS
     final int originalTileSize = 16;
     final int scale = 3;
 
 
-    final int tileSize= originalTileSize *scale;
+    final int tileSize= originalTileSize * scale;
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenRow;
+    final int screenWidth = tileSize * maxScreenRow; // 768px
+    final int screenHeight = tileSize * maxScreenRow; // 576px
 
+public GamePanel() {
+    this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+    this.setBackground(Color.black);
+    this.setDoubleBuffered(true);
+}
+    Thread gameThread;
 
+public void startGameThread(){
+    gameThread = new Thread(this);
+    gameThread.start();
+    }
 
+    @Override
+    public void run() {
+        while(gameThread !=null){
+
+            System.out.println("This game loop is running");
+            // 1 Update: update information such as character position
+            update();
+
+            // 2 Draw: Draw the screen with the updated information
+            repaint();
+        }
+}
+
+public void update(){}
+public void paintComponent(Graphics g){
+    super.paintComponent(g);
+
+    Graphics2D g2 = (Graphics2D) g;
+
+    g2.setColor(Color.white);
+
+    g2.fillRect(100, 100, tileSize, tileSize);
+
+    g2.dispose();
+
+}
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 import javax.swing.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
 
         JFrame window = new JFrame();
@@ -11,9 +10,15 @@ public class Main {
         window.setResizable(false);
         window.setTitle("Testing game");
 
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
         window.setLocationRelativeTo(null);
         window.setVisible(true);
 
+        gamePanel.startGameThread();
 
     }
 }
